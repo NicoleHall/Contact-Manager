4cf1518fea3f9da50f046510e9721e7dec58b5ce
Nicole Hall <nicolealexandrahall@gmail.com>
2015-10-31 16:01:44 -0600

    phone number responds to person.  relationship has been added.  relationship test is passing

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe PhoneNumber, type: :model do
     phone_number.number = nil
     expect(phone_number).to_not be_valid
   end
-  # 
+  #
   # it 'must have a reference to a person' do
   #   phone_number.person_id = nil
   #   expect(phone_number).not_to be_valid
   # end
+
+  it 'is associated with a person' do
+  expect(phone_number).to respond_to(:person)
+  end
 end
